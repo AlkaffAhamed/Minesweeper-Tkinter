a9aaafb9808cba3d7d0650a68639670b7ccee8f0
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-07 19:10:31 +0800

    11 Algorithm: Left click functionality - Algorithm to get the surrounding tiles    - Using the `@property` decorator - Algorithm to count the number of mines in the surrounding tiles    - Using the `@property` decorator - Completed the left click function    - Display red tile if there is a mine    - Display number of mines surrounding the tile if it is not a mine

--- cell.py
+++ cell.py
@@ -23,12 +23,52 @@ class Cell:
         self.cell_btn_object = btn
 
     def left_click_action(self, event):
-        print(event)
-        print("LEFT CLICK!")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
 
     def right_click_action(self, event):
         print(event)
         print("RIGHT CLICK!")
+
+    def show_mine(self):
+        # A Logic to do the Game Over
+        self.cell_btn_object.configure(bg="red")
+
+    def show_cell(self):
+        print(self.get_cell_by_axis(self.x, self.y), end="")
+        print(" = ", end="")
+        print(self.surround_cells)
+        print(self.surround_cells_mine_len)
+        self.cell_btn_object.configure(text=self.surround_cells_mine_len)
+
+    def get_cell_by_axis(self, x, y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    @property
+    def surround_cells(self):
+        cells = [self.get_cell_by_axis(self.x - 1, self.y - 1),
+                 self.get_cell_by_axis(self.x - 1, self.y),
+                 self.get_cell_by_axis(self.x - 1, self.y + 1),
+                 self.get_cell_by_axis(self.x, self.y - 1),
+                 self.get_cell_by_axis(self.x + 1, self.y - 1),
+                 self.get_cell_by_axis(self.x + 1, self.y),
+                 self.get_cell_by_axis(self.x + 1, self.y + 1),
+                 self.get_cell_by_axis(self.x, self.y + 1)]
+
+        cells = [c for c in cells if c is not None]
+        return cells
+
+    @property
+    def surround_cells_mine_len(self):
+        count = 0
+        for c in self.surround_cells:
+            if c.is_mine:
+                count += 1
+        return count
 
     @staticmethod
     def randomize_mines():
