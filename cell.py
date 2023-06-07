225a741779fb4e0ee9075ef6ed58289dc2437914
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-07 13:54:09 +0800

    08 Left click and right click events - Create bindings in `cell.py`: `Cell.create_btn_object()` - Create the 2 handler functions inside `Cell()` class

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
 from tkinter import Button
 
+
 class Cell:
     def __init__(self, is_mine=False):
         self.is_mine = is_mine
@@ -8,4 +9,15 @@ class Cell:
     def create_btn_object(self, location):
         btn = Button(location,
                      text="TEXT")
+        # Binding the events
+        btn.bind("<Button-1>", self.left_click_action)  # Left Click
+        btn.bind("<Button-3>", self.right_click_action)  # Right Click
         self.cell_btn_object = btn
+
+    def left_click_action(self, event):
+        print(event)
+        print("LEFT CLICK!")
+
+    def right_click_action(self, event):
+        print(event)
+        print("RIGHT CLICK!")
