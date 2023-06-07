fe004de6ecadfe8aea5d8952013eb0728a9cb2ce
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-07 13:12:42 +0800

    04 Create the center frame - Create center frame using `Frame()` - Change bg colours to black for the actual project

--- main.py
+++ main.py
@@ -12,17 +12,24 @@ root.resizable(False, False)
 
 # Title Frame
 top_frame = Frame(root,
-                  bg="red",
+                  bg="black",
                   width=settings.WIDTH,
                   height=utils.height_prct(25))
 top_frame.place(x=0, y=0)
 
 # Sidebar Frame
 left_frame = Frame(root,
-                   bg="blue",
+                   bg="black",
                    width=utils.width_prct(25),
                    height=utils.height_prct(75))
 left_frame.place(x=0, y=utils.height_prct(25))
+
+# Center Frame
+center_frame = Frame(root,
+                     bg="black",
+                     width=utils.width_prct(75),
+                     height=utils.height_prct(75))
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
 # Run Window
 root.mainloop()
