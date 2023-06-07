2a1626b67d517407ca9761d1e5bf518398d2e5ab
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-07 12:52:21 +0800

    02 Create Frames - Create 2 `Frame()` objects and place using the `place()` method

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from tkinter import *
 
-root=Tk()
+root = Tk()
+
+# Setup Window
 root.configure(bg="black")
 root.geometry("720x360")
 root.title("Minesweeper Tutorial")
-root.resizable(False,False)
-root.mainloop()
+root.resizable(False, False)
 
+# Title Frame
+top_frame = Frame(root,
+                  bg="red",
+                  width=720,
+                  height=90)
+top_frame.place(x=0, y=0)
+
+# Sidebar Frame
+left_frame = Frame(root,
+                   bg="red",
+                   width=720,
+                   height=90)
+left_frame.place(x=0, y=90)
+
+# Run Window
+root.mainloop()
