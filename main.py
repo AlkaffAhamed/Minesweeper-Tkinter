5fe83b93ee64f0a5301976ee28b7f02618b6ea3a
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-07 13:30:11 +0800

    05 Create the Cell Class - Create a new class `Cell()` and add a button inside the cell - Create cells in `main.py`

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils
 
@@ -30,6 +31,14 @@ center_frame = Frame(root,
                      width=utils.width_prct(75),
                      height=utils.height_prct(75))
 center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+
+c1 = Cell()
+c1.create_btn_object(center_frame)
+c1.cell_btn_object.place(x=0, y=0)
+
+c2 = Cell()
+c2.create_btn_object(center_frame)
+c2.cell_btn_object.place(x=40, y=0)
 
 # Run Window
 root.mainloop()
